1867870b6c78d94b26ad868217f2e956adc31287
Lukáš Norek <norekluk@fit.cvut.cz>
2024-02-20 11:14:46 +0100

    feature: compute position on screen by percentage during screen orientation changes

--- app/src/main/java/com/example/digiteqentrytask/BubbleService.kt
+++ app/src/main/java/com/example/digiteqentrytask/BubbleService.kt
@@ -2,26 +2,38 @@ package com.example.digiteqentrytask
 
 import android.annotation.SuppressLint
 import android.app.Service
+import android.content.Context
 import android.content.Intent
+import android.content.res.Configuration
 import android.graphics.PixelFormat
 import android.os.Build
 import android.os.IBinder
+import android.util.DisplayMetrics
+import android.util.Log
 import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.MotionEvent
 import android.view.WindowManager
 import com.example.digiteqentrytask.databinding.BubbleLayoutBinding
+import kotlin.math.roundToInt
+
+const val BUBBLE_START_POSITION_Y_PERCENTAGE = 0.33
 
 class BubbleService : Service() {
 
     private lateinit var binding: BubbleLayoutBinding
     private lateinit var windowManager: WindowManager
+    private var portraitWidthPx = 0
+    private var portraitHeightPx = 0
+    private var landscapeWidthPx = 0
+    private var landscapeHeightPx = 0
 
     private var lastAction = 0
     private var initialX = 0
     private var initialY = 0
     private var initialTouchX = 0f
     private var initialTouchY = 0f
+    private lateinit var params: WindowManager.LayoutParams
 
     override fun onBind(intent: Intent): IBinder {
         TODO("Return the communication channel to the service.")
@@ -30,6 +42,20 @@ class BubbleService : Service() {
     @SuppressLint("ClickableViewAccessibility")
     override fun onCreate() {
         super.onCreate()
+        Log.d(BubbleService::class.java.simpleName, "onCreate")
+        val bubbleStartPositionY =
+            (BUBBLE_START_POSITION_Y_PERCENTAGE * this.resources.configuration.screenHeightDp.dpToPx(
+                this
+            )).roundToInt()
+        Log.d(BubbleService::class.java.simpleName, "start pos y: $bubbleStartPositionY")
+
+        if (this.resources.configuration.orientation == Configuration.ORIENTATION_PORTRAIT) {
+            portraitHeightPx = this.resources.configuration.screenHeightDp.dpToPx(this)
+            portraitWidthPx = this.resources.configuration.screenWidthDp.dpToPx(this)
+        } else {
+            landscapeHeightPx = this.resources.configuration.screenHeightDp.dpToPx(this)
+            landscapeWidthPx = this.resources.configuration.screenWidthDp.dpToPx(this)
+        }
 
         binding = BubbleLayoutBinding.inflate(LayoutInflater.from(this))
         val view = binding.root
@@ -40,23 +66,26 @@ class BubbleService : Service() {
             WindowManager.LayoutParams.TYPE_PHONE
         }
 
-        val params = WindowManager.LayoutParams(
+        params = WindowManager.LayoutParams(
             WindowManager.LayoutParams.WRAP_CONTENT,
             WindowManager.LayoutParams.WRAP_CONTENT,
             type,
             WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE,
             PixelFormat.TRANSLUCENT
         )
-            .apply {// set initial position of the bubble
-                gravity = Gravity.TOP or Gravity.START
+            .apply {// set initial position of the bubble, if gravity is
+                // used x and y are and offset from that position in direction of screen space
+                // if gravity is TOP END x goes left and y down
+                gravity = Gravity.TOP or Gravity.END
                 x = 0
-                y = 100
+                y = bubbleStartPositionY
             }
 
         // update view position based on touch input
         view.setOnTouchListener { _, event ->
             when (event.action) {
                 MotionEvent.ACTION_DOWN -> {
+
 
                     //remember the initial position.
                     initialX = params.x
@@ -74,7 +103,11 @@ class BubbleService : Service() {
                     // Identify click action
                     if (lastAction == MotionEvent.ACTION_DOWN) {
                         //Open MainActivity on bubble click
-                        startActivity(Intent(this@BubbleService, MainActivity::class.java))
+                        startActivity(
+                            Intent(this@BubbleService, MainActivity::class.java).addFlags(
+                                Intent.FLAG_ACTIVITY_NEW_TASK
+                            )
+                        )
                         //close the service and remove the bubble
                         stopSelf()
                     }
@@ -84,9 +117,15 @@ class BubbleService : Service() {
 
                 MotionEvent.ACTION_MOVE -> {
                     //Calculate the X and Y coordinates of the view.
-                    params.x = initialX + (event.rawX - initialTouchX).toInt()
-                    params.y = initialY + (event.rawY - initialTouchY).toInt()
-
+                    //Computes relative to WindowManager.LayoutParams.gravity. If it is END
+                    // we have to use subtraction.
+                    //In case of START we have to use addition
+                    params.x = (initialX - (event.rawX - initialTouchX).toInt()).coerceAtLeast(0)
+                    //Computes relative to WindowManager.LayoutParams.gravity. If it is BOTTOM
+                    // we have to use subtraction.
+                    //In case of TOP we have to use addition
+                    params.y = (initialY + (event.rawY - initialTouchY)).toInt().coerceAtLeast(0)
+                    Log.d(BubbleService::class.java.simpleName, "${params.x}, ${params.y}")
                     //Update the layout with new X & Y coordinate
                     windowManager.updateViewLayout(view, params)
                     lastAction = event.action
@@ -106,4 +145,68 @@ class BubbleService : Service() {
         windowManager.removeView(binding.root)
     }
 
+    override fun onConfigurationChanged(newConfig: Configuration) {
+
+        if (newConfig.orientation == Configuration.ORIENTATION_PORTRAIT && (portraitWidthPx == 0 || portraitHeightPx == 0)) {
+            portraitWidthPx = this.resources.configuration.screenWidthDp.dpToPx(this)
+            portraitHeightPx = this.resources.configuration.screenHeightDp.dpToPx(this)
+        }
+
+        if (newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE && (landscapeWidthPx == 0 || landscapeHeightPx == 0)) {
+            landscapeWidthPx = this.resources.configuration.screenWidthDp.dpToPx(this)
+            landscapeHeightPx = this.resources.configuration.screenHeightDp.dpToPx(this)
+        }
+
+        val orientationString = if (newConfig.orientation == 1) "portrait" else "landscape"
+        Log.d(BubbleService::class.java.simpleName, orientationString)
+        Log.d(
+            BubbleService::class.java.simpleName,
+            "config width: ${newConfig.screenWidthDp}, height: ${newConfig.screenHeightDp}"
+        )
+        Log.d(
+            BubbleService::class.java.simpleName,
+            "config width: ${newConfig.screenWidthDp.dpToPx(this)}, height: ${newConfig.screenHeightDp.dpToPx(this)}"
+        )
+        Log.d(
+            BubbleService::class.java.simpleName,
+            "portrait width: ${portraitWidthPx}, height: ${portraitHeightPx}"
+        )
+        Log.d(
+            BubbleService::class.java.simpleName,
+            "landscape width: ${landscapeWidthPx}, height: ${landscapeHeightPx}"
+        )
+
+        params.x = computeNewPositionInPxByPercentage(
+            params.x,
+            if (newConfig.orientation == Configuration.ORIENTATION_PORTRAIT) landscapeWidthPx else portraitWidthPx,
+            newConfig.screenWidthDp.dpToPx(this),
+        )
+
+        params.y = computeNewPositionInPxByPercentage(
+            params.y,
+            if (newConfig.orientation == Configuration.ORIENTATION_PORTRAIT) landscapeHeightPx else portraitHeightPx,
+            newConfig.screenHeightDp.dpToPx(this),
+        )
+
+        Log.d(
+            BubbleService::class.java.simpleName,
+            "computed params x: ${params.x}, y: ${params.y}"
+        )
+        windowManager.updateViewLayout(binding.root, params)
+    }
+}
+
+fun Float.pxToDp(context: Context): Int =
+    (this / (context.resources.displayMetrics.densityDpi.toFloat() / DisplayMetrics.DENSITY_DEFAULT)).roundToInt()
+
+fun Int.dpToPx(context: Context): Int = (this * context.resources.displayMetrics.density).roundToInt()
+
+fun computeNewPositionInPxByPercentage(
+    coordinate: Int,
+    originalScreenSize: Int,
+    newScreenSize: Int,
+): Int {
+    val originalPositionXPercentageToScreen = coordinate.toFloat() / originalScreenSize
+    val finalPositionX = originalPositionXPercentageToScreen * newScreenSize
+    return finalPositionX.roundToInt()
 }
